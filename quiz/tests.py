66a67640f4e0d2244c42d13d8b6226bfc6095f5e
soo <tnalsdh21@gmail.com>
2022-06-29 06:19:15 +0900

    Ready for deploy

---
+++ quiz/tests.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+
+# Create your tests here.
+from django.db import models
+
+
+class Quiz(models.Model):
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    answer = models.IntegerField()
